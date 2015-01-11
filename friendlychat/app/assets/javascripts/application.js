3ddf10b9460cc8955db9570f10ac02e73935eb4d
Robert Wildman <Robertwildman4@gmail.com>
2015-01-11 18:04:37 +0000

    Revert 5082359..1ea8f73
    This rolls back to commit 50823599faf2b817305b4f83af1b5809e47e6597.

--- friendlychat/app/assets/javascripts/application.js
+++ friendlychat/app/assets/javascripts/application.js
@@ -13,6 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require private_pub
 //= require_tree .
-
--- friendlychat/app/controllers/chats_controller.rb
+++
@@ -1,7 +0,0 @@
-class ChatsController < ApplicationController
-  def create
-    @chat = Chat.new(params[:content])
-    PrivatePub.publish_to("/messages/new", "alert('#{@chat.content}');")
-    render 'create'
-  end
-end
