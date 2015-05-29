e0b2a455b5a1830b68e9f62b2b39df6766da5d38
Robert Wildman <robertwildman@Roberts-iMac-4.default>
2015-05-29 17:04:51 +0100

    Starting work on the splash screen

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
+Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
 Rails.application.config.assets.precompile += %w( messages.css )
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  root 'messages#index'
+  root 'messages#splash'
   get '/testroom' => 'messages#testroom'
   get '/getstartinfo' => 'messages#getstartinfo'
   post '/new_message' => 'messages#new_message', :as => :new_message
